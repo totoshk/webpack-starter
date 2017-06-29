d42066471d397a0da0d4ac3426f381a1138e5409
Sergey <johndoe@example.com>
2017-06-29 22:58:59 +0300

    Добавлен пакет eslint и файл с правилами eslintrc, а также editorconfig

--- app/index.js
+++ app/index.js
@@ -1,9 +1,5 @@
 require('./scss/index.scss');
-
 const myArr = [1, 2, 3, 4, 5, 6];
-
-myArr.forEach(x => console.log(x));
-
-let newArr = myArr.map(x => x*x);
-
-console.log(newArr);
+const newArr = myArr.map(x => {
+  return x * x;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                use: 'babel-loader'
+                use: ['babel-loader', 'eslint-loader']
             },
             {
                 test: /\.scss$/, 
